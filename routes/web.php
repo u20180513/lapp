806bf281e553bbdb758500d3ad8f24ef77c9f1a0
u20180513 <second111sak@gmail.com>
2018-09-26 10:48:52 +0900

    create laravel project

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function () {
+
+    $html = '<html>
+                <body>
+                    <h1>Hello</h1>
+                    <p>This is sample page.</p>
+                </body>
+             </html>';
+
+    return $html;
+});
